213210691460d0f6ccfdd7fd01f49bf8592e6240
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-01-12 18:58:53 +0300

    Create MaasHesaplama.Application.java

---
+++ MaasHesaplama.Application.java
@@ -0,0 +1,12 @@
+package com.main;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee("kemal",2000, 45, 1985);
+		
+		System.out.println(emp.toString());
+	}
+
+}
