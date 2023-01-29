51c6a7a057b7568faa15c97b41c1affa2858912f
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-01-29 23:59:20 +0300

    Create MonsterGAME.App.java

---
+++ MonsterGAME.App.java
@@ -0,0 +1,19 @@
+public class App {
+	public static void main(String[] args) {
+		Game game = new Game();
+		// game.startGame();
+
+		Attack attack = new SuperAttack();
+		System.out.println(attack.addNumbers(5, 4));
+		System.out.println(Attack.multiplyNumbers(2, 3));
+
+//		Player player = new Player("Ayse Oner", 20);
+//		player.addTool(new Stick());
+//		player.addTool(new Cannon());
+//		player.addTool(new Catapult());
+//		player.addTool(new Stick());
+//		player.printTools();
+//		player.refillTools();
+	}
+
+}
