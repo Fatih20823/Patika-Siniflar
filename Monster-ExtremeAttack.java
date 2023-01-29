580c43a92ee4fbaba48f2022c928a296761b8986
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-01-30 00:01:49 +0300

    Create Monster-ExtremeAttack.java

---
+++ Monster-ExtremeAttack.java
@@ -0,0 +1,11 @@
+package com.fatih;
+
+public class ExtremeAttack implements Attack {
+
+	@Override
+	public int calculateAttackPoints(int attackPoints) {
+		System.out.println("EXTREME ATTACK");
+		return attackPoints * 3;
+	}
+
+}
