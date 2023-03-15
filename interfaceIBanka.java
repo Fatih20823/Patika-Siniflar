bd085d634edfcd615271a50c5ac7a8d4ed2d4e9d
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-03-15 13:37:34 +0300

    Create interfaceIBanka.java

---
+++ interfaceIBanka.java
@@ -0,0 +1,7 @@
+public interface IBanka {
+	
+	final String hostIpAdress = "127.0.0.1";
+	
+	boolean connect(String ipAddress);
+	boolean payment(double price, String cardNumber, String date, String cvc);
+}
